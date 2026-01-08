1043b4b87185ae1037cd707bfbc0f633c58782e2
EvgenyDantsov <adriano-sport@mail.ru>
2026-01-09 00:35:29 +0300

    add locators and actions to LoginPage

---
+++ src/main/java/page/LoginPage.java
@@ -0,0 +1,40 @@
+package page;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage {
+    private final By usernameInputField=By.id("user-name");
+    private final By passwordInputField=By.id("password");
+    private final By loginButton=By.id("login-button");
+    private final By errorMessage =By.xpath("//h3[@data-test='error']");
+    private final WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    @Step("Login with username: {0} and password: {1}")
+    public void login(String username, String password) {
+        driver.findElement(usernameInputField).sendKeys(username);
+        driver.findElement(passwordInputField).sendKeys(password);
+        clickLoginButton();
+    }
+    public void clickLoginButton() {
+        WebElement loginBtn = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(loginButton));
+        loginBtn.click();
+    }
+    public boolean isErrorMessageDisplayed() {
+        return !driver.findElements(errorMessage).isEmpty()
+                && driver.findElement(errorMessage).isDisplayed();
+    }
+
+    public boolean isProductsPageOpened() {
+        return driver.getCurrentUrl().contains("inventory");
+    }
+}

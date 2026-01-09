9814ce085b3c113f553d7b936738d9a128f64eb7
Evgeny Dantsov <adriano-sport@mail.ru>
2026-01-09 15:39:54 +0300

    add driver paths to WebDriverUtil and config.properties

--- src/main/java/factory/WebDriverUtil.java
+++ src/main/java/factory/WebDriverUtil.java
@@ -5,13 +5,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class WebDriverUtil {
+    private static Properties loadConfig() {
+        Properties config = new Properties();
+        try (InputStream input = WebDriverUtil.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            config.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка загрузки config.properties", e);
+        }
+        return config;
+    }
     public static WebDriver getWebDriver(String browser) {
+        Properties config = loadConfig();
         WebDriver driver = null;
         // Используем конструкцию switch для выбора нужного браузера в котором тестируем
         switch (browser.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", config.getProperty("chrome.driver"));
                 driver = new ChromeDriver();
                 break;
             case "firefox":

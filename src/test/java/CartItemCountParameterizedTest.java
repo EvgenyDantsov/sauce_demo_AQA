2151ea5821039173ee0e1f87eab14784a1d0a2b7
EvgenyDantsov <adriano-sport@mail.ru>
2026-01-13 00:16:24 +0300

    add parameterized test to verify cart item count updates correctly when adding products

---
+++ src/test/java/CartItemCountParameterizedTest.java
@@ -0,0 +1,61 @@
+import factory.WebDriverUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import page.LoginPage;
+import page.ProductsPage;
+import util.BaseTest;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class CartItemCountParameterizedTest extends BaseTest {
+    private static final String BROWSER = "chrome";
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    private final List<String> productsToAdd;
+    private final int expectedCount;
+    private WebDriver driver;
+
+    public CartItemCountParameterizedTest(List<String> productsToAdd, int expectedCount) {
+        this.productsToAdd = productsToAdd;
+        this.expectedCount = expectedCount;
+    }
+
+    @Parameterized.Parameters(name = "{index}: adding {0} -> expected count={1}")
+    public static Object[][] getData() {
+        return new Object[][]{
+                {List.of("Sauce Labs Backpack"), 1},
+                {List.of("Sauce Labs Backpack", "Sauce Labs Bike Light"), 2},
+                {List.of("Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt"), 3}
+        };
+    }
+
+    @Before
+    public void webDriver() {
+        driver = WebDriverUtil.getWebDriver(BROWSER);
+        driver.get(BASE_URL);
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+    }
+
+    @Test
+    public void cartItemCountTest() {
+        for (String product : productsToAdd) {
+            productsPage.clickProductActionButton(product);
+        }
+        assertEquals(expectedCount, productsPage.getCartItemsCount());
+    }
+
+    @After
+    public void quitBrowser() {
+        // Закрываем браузер
+        WebDriverUtil.quitDriver(driver);
+    }
+}

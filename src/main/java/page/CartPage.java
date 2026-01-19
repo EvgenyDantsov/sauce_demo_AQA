746fca05b9602d45aa63470b621ab356cb6d4156
Evgeny Dantsov <adriano-sport@mail.ru>
2026-01-19 13:54:16 +0300

    Add Allure steps to CartPage actions

--- src/main/java/page/CartPage.java
+++ src/main/java/page/CartPage.java
@@ -1,5 +1,6 @@
 package page;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -11,23 +12,30 @@ import java.time.Duration;
 
 public class CartPage {
     private final By cartTitle = By.xpath("//span[@data-test='title']");
-    private static final String PRODUCT_ACTION_BUTTON_XPATH ="//div[@class='cart_item'][.//div[text()='%s']]//button";
-    private final By shoppingCartBadge=By.xpath("//span[@data-test='shopping-cart-badge']");
+    private static final String PRODUCT_ACTION_BUTTON_XPATH = "//div[@class='cart_item'][.//div[text()='%s']]//button";
+    private final By shoppingCartBadge = By.xpath("//span[@data-test='shopping-cart-badge']");
+    private final By checkoutButton = By.id("checkout");
     private final WebDriver driver;
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    @Step("Get the title of the cart page")
     public String getCartTitle() {
         return driver.findElement(cartTitle).getText();
     }
+
     private By productRemoveButton(String productName) {
         return By.xpath(String.format(PRODUCT_ACTION_BUTTON_XPATH, productName));
     }
+
+    @Step("Get the text of the remove button for product: {productName}")
     public String getRemoveButtonText(String productName) {
         return driver.findElement(productRemoveButton(productName)).getText();
     }
+
+    @Step("Click 'Remove' button for product: {productName}")
     public void clickRemoveButton(String productName) {
         WebElement productActionButton = new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.elementToBeClickable(
@@ -35,6 +43,15 @@ public class CartPage {
                 ));
         productActionButton.click();
     }
+
+    @Step("Click the 'Checkout' button in the cart")
+    public void clickCheckoutButton() {
+        WebElement checkoutBtn = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(checkoutButton));
+        checkoutBtn.click();
+    }
+
+    @Step("Get the number of items in the cart")
     public int getCartItemsCount() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 

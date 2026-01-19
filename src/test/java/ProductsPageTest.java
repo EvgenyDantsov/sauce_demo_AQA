86aeec18d713b8498cce803e76cd7fc53c873aad
Evgeny Dantsov <adriano-sport@mail.ru>
2026-01-19 13:59:53 +0300

    Add test exposing bug where Reset App State does not reset product buttons

--- src/test/java/ProductsPageTest.java
+++ src/test/java/ProductsPageTest.java
@@ -1,22 +1,31 @@
+import enums.SortProducts;
 import factory.WebDriverUtil;
+import io.qameta.allure.Issue;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import page.CartPage;
+import page.HeaderMenu;
 import page.LoginPage;
 import page.ProductsPage;
 import util.BaseTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ProductsPageTest extends BaseTest {
     private static final String BROWSER = "chrome";
+    private static final String PRODUCT_NAME = "Sauce Labs Backpack";
     private WebDriver driver;
     LoginPage loginPage;
     ProductsPage productsPage;
     CartPage cartPage;
+    HeaderMenu headerMenu;
 
     @Before
     public void webDriver() {
@@ -25,6 +34,7 @@ public class ProductsPageTest extends BaseTest {
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
+        headerMenu = new HeaderMenu(driver);
         loginPage.login("standard_user", "secret_sauce");
     }
 
@@ -49,28 +59,49 @@ public class ProductsPageTest extends BaseTest {
     @Test
     @DisplayName("Product price has correct format $XX.XX")
     public void productsPriceHasCorrectFormatTest() {
-        assertTrue(productsPage.isProductPriceFormatValid("Sauce Labs Backpack"));
+        assertTrue(productsPage.isProductPriceFormatValid(PRODUCT_NAME));
     }
 
     @Test
     @DisplayName("Product button text changes to 'Remove' after adding to cart")
     public void removeProductForCartFromProductsPageTest() {
-        productsPage.clickProductActionButton("Sauce Labs Backpack");
-        assertEquals("Remove", productsPage.getActionButtonText("Sauce Labs Backpack"));
+        productsPage.clickProductActionButton(PRODUCT_NAME);
+        assertEquals("Remove", productsPage.getActionButtonText(PRODUCT_NAME));
     }
 
     @Test
     @DisplayName("Button text toggles between 'Add to cart' and 'Remove' when product is added/removed")
     public void buttonTextTogglesOnAddRemoveTest() {
-        productsPage.clickProductActionButton("Sauce Labs Backpack");
-        productsPage.clickProductActionButton("Sauce Labs Backpack");
-        assertEquals("Add to cart", productsPage.getActionButtonText("Sauce Labs Backpack"));
+        productsPage.clickProductActionButton(PRODUCT_NAME);
+        productsPage.clickProductActionButton(PRODUCT_NAME);
+        assertEquals("Add to cart", productsPage.getActionButtonText(PRODUCT_NAME));
     }
 
     @Test
     @DisplayName("Footer should be visible on Products page")
     public void footerIsDisplayedOnProductPageTest() {
         assertTrue(productsPage.isFooterDisplayed());
+    }
+
+    @Issue("SAUCE-RESET-001")
+    @Test
+    @DisplayName("Reset App State clears cart and resets product buttons to 'Add to cart'")
+    public void resetAppStateShouldClearCartAndResetProductButtonTest() {
+        productsPage.clickProductActionButton(PRODUCT_NAME);
+        headerMenu.clickMenuButton();
+        headerMenu.clickResetAppState();
+        assertEquals(0, productsPage.getCartItemsCount());
+        assertEquals("Add to cart", productsPage.getActionButtonText(PRODUCT_NAME));
+    }
+
+    @Test
+    @DisplayName("Products can be sorted by price from low to high")
+    public void productsCanBeSortedByPriceLowToHighTest() {
+        productsPage.selectSortOption(SortProducts.PRICE_LOW_TO_HIGH);
+        List<Double> prices = productsPage.getAllProductPrices();
+        List<Double> sortedPrices = new ArrayList<>(prices);
+        Collections.sort(sortedPrices);
+        assertEquals(sortedPrices, prices);
     }
 
     @After
